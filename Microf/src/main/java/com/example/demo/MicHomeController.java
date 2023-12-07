541d5dcb3d265cdc1542f0ed047698a8f141ccdc
Yanagisawa12 <tomot2779@gmail.com>
2023-12-07 10:05:27 +0900

    AP2できた

---
+++ Microf/src/main/java/com/example/demo/MicHomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicHomeController {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	//コピペ用サンプル(ページ表示用メソッド)
+	@RequestMapping(path = "/michome", method = RequestMethod.GET)
+	public String copGet(Model model) {
+		List<Map<String, Object>> resultList;
+		resultList = jdbcTemplate.queryForList("SELECT * FROM todo ");
+		model.addAttribute("resultList", resultList);
+		return "michome";
+	}
+
+
+}
