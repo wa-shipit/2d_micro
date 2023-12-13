af1809a3366bcabe3eb9d263de8823f46f472621
S0519 <S0519@21S0519>
2023-12-13 11:29:20 +0900

    AP2完成

---
+++ Microf/src/main/java/com/example/demo/MicHomeController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicHomeController {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@RequestMapping(path = "/michome", method = RequestMethod.GET)
+	public String michome(Model model) throws IOException {
+		List<Map<String, Object>> resultList;
+
+		//SELECT文の実行
+		resultList = jdbcTemplate.queryForList("select * from todo");
+
+		//実行結果をmodelにしまってHTMLで出せるようにする。
+		model.addAttribute("selectResult", resultList);
+
+		return "michome";
+	}
+
+	@RequestMapping(path = "/michome", method = RequestMethod.POST)
+	public String mainmenu()
+			throws IOException {
+
+		return "michome";
+
+	}
+}
