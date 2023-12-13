86e237740297f7c8924302b2589f1d349450de7f
S0529 <S0529@21S0529>
2023-12-13 11:27:14 +0900

    home

--- Microf/src/main/java/com/example/demo/controller/MicHomeController.java
+++ Microf/src/main/java/com/example/demo/controller/MicHomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MicHomeController {
 	@RequestMapping(path = "/michome", method = RequestMethod.GET)
-	public String copGet() {
+	public String copGet1() {
 		return "michome";
 
 	}
@@ -15,5 +15,23 @@ public class MicHomeController {
 	@RequestMapping(path = "/michome", method = RequestMethod.POST)
 	public String copPost() {
 		return "michome";
+	}
+
+	@RequestMapping(path = "/micadd", method = RequestMethod.GET)
+	public String copGet2() {
+		return "michome";
+
+	}
+
+	@RequestMapping(path = "/micedit", method = RequestMethod.GET)
+	public String copGet3() {
+		return "michome";
+
+	}
+
+	@RequestMapping(path = "/micdel", method = RequestMethod.GET)
+	public String copGet4() {
+		return "michome";
+
 	}
 }
