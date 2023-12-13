e187e73dcef64af054dd17965bb0c314e33d8e89
RaiIsono0914 <lairin0602@gmail.com>
2023-12-13 11:33:45 +0900

    完成

--- Microf/src/main/java/com/example/demo/MicLoginController.java
+++ Microf/src/main/java/com/example/demo/MicLoginController.java
@@ -10,43 +10,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class MicLoginController {
 
-	@Autowired
-	JdbcTemplate jdbcTemplate;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-	@RequestMapping(path = "/miclogin", method = RequestMethod.GET)
-	public String LoginGet() {
-		return "miclogin";
-	}
+    @RequestMapping(path = "/miclogin", method = RequestMethod.GET)
+    public String LoginGet() {
+        return "miclogin";
+    }
 
-	@RequestMapping(path = "/miclogin", method = RequestMethod.POST)
-	public String LoginPost(String miclogin, String micpw, Model model) {
+    @RequestMapping(path = "/miclogin", method = RequestMethod.POST)
+    public String LoginPost(String miclogin, String micpw, Model model, HttpSession session) {
+        int length = miclogin.length();
+        int length2 = micpw.length();
+        if (length < 17 && length2 < 17) {
 
+            List<Map<String, Object>> resultList = jdbcTemplate
+                    .queryForList("SELECT * FROM miclogin WHERE loginid=? AND password=?", miclogin, micpw);
 
+            model.addAttribute("miclogin", miclogin);
+            model.addAttribute("micpw", micpw);
 
-		int length = miclogin.length();
-		int length2 = micpw.length();
-		if (length < 17 && length2 < 17) {
+            int count = resultList.size();
 
-			List<Map<String, Object>> resultList = jdbcTemplate
-					.queryForList("SELECT * FROM miclogin WHERE loginid=? AND password=?",miclogin,micpw);
+            if (count > 0) {
+                // ログイン成功時にセッションに情報を保存
+                session.setAttribute("loginparam1", miclogin);
+                session.setAttribute("loginparam2", micpw);
+                return "redirect:/michome";
+            } else {
+                return "miclogin_ng";
+            }
 
-			model.addAttribute("miclogin", miclogin);
-			model.addAttribute("micpw", micpw);
-
-			int count = resultList.size();
-
-			if (count > 0) {
-				return "redirect:/michome";
-			} else {
-				return "miclogin_ng";
-			}
-
-		} else {
-			return "miclogin_ng2";
-		}
-
-	}
+        } else {
+            return "miclogin_ng2";
+        }
+    }
 }
