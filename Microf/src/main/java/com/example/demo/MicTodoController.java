d9d28d8509ac4fd62fcc0be03df89933f205788f
G0943 <G0943@DESKTOP-DKP1SJN>
2023-12-13 12:49:07 +0900

    あ

--- Microf/src/main/java/com/example/demo/MicTodoController.java
+++ Microf/src/main/java/com/example/demo/MicTodoController.java
@@ -15,33 +15,34 @@ public class MicTodoController {
 	JdbcTemplate jdbcTemplate;
 
 	//コピペ用サンプル(ページ表示用メソッド)
-	@RequestMapping(path = "/mictodo_add", method = RequestMethod.GET)
+	@RequestMapping(path = "/micadd", method = RequestMethod.GET)
 	public String addGet() {
-		return "micadd";
+		List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT * FROM todo   ");
+		return "mictodo_add";
 	}
 
 	//コピペ用サンプル（画面から何か入力をした時用）
-	@RequestMapping(path = "/mictodo_add", method = RequestMethod.POST)
+	@RequestMapping(path = "/micadd", method = RequestMethod.POST)
 	public String addPost(	int add1, int add2,String add3, Model model) {
 
 		//DBに繋ぐならこんな感じ(JdbcTemplate)
-			List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT * FROM todo WHERE  ");
+			
             
-			jdbcTemplate.update("insert into todo(month,day,todo) VALUES (?,?,?)",1,1,"しょうがつ");
+			jdbcTemplate.update("INSERT INTO todo VALUES (?,?);",1 ,1,"あ" );
 		model.addAttribute("add1", add1);
 		model.addAttribute("add2",add2);
 		model.addAttribute("add3",add3);
 
-		return "micadd";
+		return "mictodo_add";
 	}
 	
-	@RequestMapping(path = "/mictodo_dit", method = RequestMethod.GET)
+	@RequestMapping(path = "/micdit", method = RequestMethod.GET)
 	public String ditGet() {
-		return "micedit";
+		return "mictodo_dit";
 	}
 
 	//コピペ用サンプル（画面から何か入力をした時用）
-	@RequestMapping(path = "/mictodo_dit", method = RequestMethod.POST)
+	@RequestMapping(path = "/micdit", method = RequestMethod.POST)
 	public String ditPost(String dit1, String dit2,String dit3, Model model) {
 
 		//DBに繋ぐならこんな感じ(JdbcTemplate)
@@ -51,15 +52,15 @@ public class MicTodoController {
 		model.addAttribute("dit2", dit2);
 		model.addAttribute("dit3", dit3);
 
-		return "micedit";
+		return "mictodo_dit";
 	}
-	@RequestMapping(path = "/mictodo_del", method = RequestMethod.GET)
+	@RequestMapping(path = "/micdel", method = RequestMethod.GET)
 	public String delGet() {
 		return "mictodo_del";
 	}
 
 	//コピペ用サンプル（画面から何か入力をした時用）
-	@RequestMapping(path = "/mictodo_del", method = RequestMethod.POST)
+	@RequestMapping(path = "/micdel", method = RequestMethod.POST)
 	public String delPost(String del1, String del2, Model model) {
 
 		//DBに繋ぐならこんな感じ(JdbcTemplate)
