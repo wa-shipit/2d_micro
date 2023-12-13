f2e331b53df7f66da681697178a511b519162df6
S0524 <S0524@21S0524>
2023-12-13 12:25:23 +0900

    ap4

---
+++ Microf/src/main/java/com/example/demo/MicUserController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MicUserController {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	//コピペ用サンプル(ページ表示用メソッド)
+	@RequestMapping(path = "/micuser", method = RequestMethod.GET)
+	public String copGet() {
+		return "micuser";
+	}
+
+	//コピペ用サンプル（画面から何か入力をした時用）
+	@RequestMapping(path = "/micuser", method = RequestMethod.POST)
+	public String copPost(String loginid, String password, Model model) {
+
+		//DBに繋ぐならこんな感じ(JdbcTemplate)
+		List<Map<String, Object>> resultList = jdbcTemplate.queryForList("SELECT * FROM miclogin WHERE loginid = ? AND password = ?", loginid, password);
+
+		// 既存のデータがない場合にのみINSERT文を実行
+	    if (resultList.isEmpty()) {
+		jdbcTemplate.update("INSERT INTO miclogin(loginid,password) values(?, ?)",loginid,password);
+	    }
+		model.addAttribute("loginid", loginid);
+		model.addAttribute("password", password);
+
+		return "micuser";
+	}
+}
